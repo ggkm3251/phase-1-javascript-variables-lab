339c45d2f0689b4f2f0bef7768b4fd55d350a87e
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-12 11:59:53 +0300

    commit tree 3cbe2740a9500e1e38e303cdb48c67f9c2a61f25

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+let companyName = 'Scuber'
+
+const js = '/const companyName/, "Expected companyName to be a const'
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
